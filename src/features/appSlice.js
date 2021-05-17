1a208755e2cb0129e10eaac5b560de5cc1695ee5
AndreiAnisia <anisia_andrei_88@yahoo.com>
2021-05-17 11:40:03 +0300

    Added Readme

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const appSlice = createSlice({
-   name: 'user',
+   name: 'app',
    initialState: {
       channelId: null,
       channelName: null,
